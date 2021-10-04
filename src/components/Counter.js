abe80e9f1a841bff48dcad85aec2ede3b8b08055
Derrick Strong <derrickmstrong@gmail.com>
2021-10-04 15:02:41 -0500

    Format changes

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,14 +5,13 @@ import { CounterActions } from '../store';
 
 const Counter = () => {
   const count = useSelector(state => state.counter.count);
-
   const dispatch = useDispatch();
 
   return (
     <div>
       <h1>Counter</h1>
-      <button onClick={() => dispatch(CounterActions.decrement())}>-</button> 
-      {count}{' '}
+      <button onClick={() => dispatch(CounterActions.decrement())}>-</button>
+      {count}
       <button onClick={() => dispatch(CounterActions.increment())}>+</button>
     </div>
   );
